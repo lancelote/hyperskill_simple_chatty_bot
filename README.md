0db21bc12c407204140ae9541eb813cc5fe4f57c
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-02-07 14:55:50 +0300

    Stage 1 is complete

---
+++ README.md
@@ -0,0 +1,8 @@
+# hyperskill_simple_chatty_bot
+
+"Simple Chatty Bot" project from [Hyperskill](https://hyperskill.org/).
+
+- [x] Stage 1: Hello! What's your name?
+- [ ] Stage 2: How old are you?
+- [ ] Stage 3: Let's count!
+- [ ] Stage 4: The student and the teacher
---
+++ src/stage1/project/SimpleBot.java
@@ -0,0 +1,11 @@
+package stage1.project;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        String botName = "Aid";
+        Integer birthYear = 2018;
+
+        System.out.printf("Hello! My name is %s.\n", botName);
+        System.out.printf("I was created in %s", birthYear);
+    }
+}
