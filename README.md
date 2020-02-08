63a7853fdf652279a148d465daddddb5b0cd39c2
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-02-08 09:25:09 +0300

    Stage 2 is complete

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 "Simple Chatty Bot" project from [Hyperskill](https://hyperskill.org/).
 
 - [x] Stage 1: Hello! What's your name?
-- [ ] Stage 2: How old are you?
+- [x] Stage 2: How old are you?
 - [ ] Stage 3: Let's count!
 - [ ] Stage 4: The student and the teacher
---
+++ src/stage2/project/SimpleBot.java
@@ -0,0 +1,26 @@
+package stage2.project;
+
+import java.util.Scanner;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.next();
+
+        System.out.printf("What a great name you have, %s!\n", name);
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int reminder3 = scanner.nextInt();
+        int reminder5 = scanner.nextInt();
+        int reminder7 = scanner.nextInt();
+        int age = (reminder3 * 70 + reminder5 * 21 + reminder7 * 15) % 105;
+
+        System.out.printf("Your age is %s; that's a good time to start programming!\n", age);
+    }
+}
