0fe4860d88d1d85e5e5cdfa2607748337f7ea2dc
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-02-08 21:07:55 +0300

    Stage 4 Homework 6

---
+++ src/stage4/hw6/Movie.java
@@ -0,0 +1,29 @@
+package stage4.hw6;
+
+class Movie {
+    private String title;
+    private String desc;
+    private int year;
+
+    public Movie(String title, String desc, int year) {
+        this.title = title;
+        this.desc = desc;
+        this.year = year;
+    }
+
+    public Movie(String title, int year) {
+        this(title, "empty", year);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
