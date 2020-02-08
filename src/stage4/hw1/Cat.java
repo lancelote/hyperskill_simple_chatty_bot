768529457aa93197c339a39c8f3689eaae7163cd
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-02-08 16:45:23 +0300

    Stage 4 Homework 1

---
+++ src/stage4/hw1/Cat.java
@@ -0,0 +1,22 @@
+package stage4.hw1;
+
+class Cat {
+
+    String name;
+    int age;
+    static int counter = 0;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+        counter++;
+
+        if (counter > 5) {
+            System.out.println("You have too many cats");
+        }
+    }
+
+    public static int getNumberOfCats() {
+        return counter;
+    }
+}
