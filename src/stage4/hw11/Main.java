76751fce6ac4e6a7b42930e7771462360ead41fd
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-02-12 17:38:26 +0300

    Stage 4 Homework 11

---
+++ src/stage4/hw11/Main.java
@@ -0,0 +1,13 @@
+package stage4.hw11;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(toPrimitive(Boolean.valueOf(true)));
+        System.out.println(toPrimitive(Boolean.valueOf(false)));
+        System.out.println(toPrimitive(null));
+    }
+
+    public static boolean toPrimitive(Boolean b) {
+        return b == null ? false : b;
+    }
+}
