1975679209eb59d3121164811780a55dde9ad3aa
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-01-06 13:52:20 +0300

    Update stage 3 project

--- src/stage3/project/SimpleBot.java
+++ src/stage3/project/SimpleBot.java
@@ -7,12 +7,13 @@ public class SimpleBot {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2018.");
+        System.out.println("I was created in 2021.");
         System.out.println("Please, remind me your name.");
 
         String name = scanner.next();
 
         System.out.printf("What a great name you have, %s!\n", name);
+
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
